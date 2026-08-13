d6cfeba5a38d8c26e05507a4ca44ab1d217fc816
BlazingSid <attarshahid62@gmail.com>
2026-08-14 00:07:40 +0530

    commit

--- OOPs/PyAssignmentQ8.py
+++ OOPs/PyAssignmentQ8.py
@@ -1,3 +1,18 @@
 """8. Object Count Tracker: Design a class that tracks how many objects
 have been created from it and has a method to display this count"""
 
+class Object:
+    count = 0
+
+    def __init__(self):
+        Object.count += 1
+
+    def display_count(self):
+        print("Number of objects:", Object.count)
+
+
+obj1 = Object()
+obj2 = Object()
+obj3 = Object()
+
+obj3.display_count()
